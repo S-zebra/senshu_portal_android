4279ff0fca5bb04924ff1cb49a0d430d2c5fcb00
S-zebra <shimashimazebra13@gmail.com>
2020-02-29 17:09:16 +0900

    Drawerが動くようにした

--- app/src/main/java/szebra/senshu_timetable/activities/MainActivity.java
+++ app/src/main/java/szebra/senshu_timetable/activities/MainActivity.java
@@ -1,23 +1,92 @@
 package szebra.senshu_timetable.activities;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.HashMap;
 
 import szebra.senshu_timetable.R;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+  private ActionBarDrawerToggle drawerToggle;
+  private DrawerLayout drawer;
+  private HashMap<Integer, Fragment> fragmentsMap;
   
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    NavigationView nv = findViewById(R.id.nav_view);
+    nv.setNavigationItemSelectedListener(this);
+  
+    Toolbar toolbar = findViewById(R.id.toolbar_main);
+    setSupportActionBar(toolbar);
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+  
+    drawer = findViewById(R.id.drawer_layout);
+    drawerToggle = new ActionBarDrawerToggle(this, drawer, R.string.hint_drawer_open, R.string.hint_drawer_close);
+    drawerToggle.syncState();
+  
+    fragmentsMap = new HashMap<>();
+    switchFragment(R.id.drawer_item_timetable);
+  }
+  
+  @Override
+  protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+    super.onPostCreate(savedInstanceState);
+    drawerToggle.syncState();
+  }
+  
+  @Override
+  public void onConfigurationChanged(Configuration newConfig) {
+    super.onConfigurationChanged(newConfig);
+    drawerToggle.syncState();
+  }
+  
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    if (drawerToggle.onOptionsItemSelected(item)) {
+      return true;
+    }
+    return super.onOptionsItemSelected(item);
   }
   
   @Override
   public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-    return false;
+    super.onOptionsItemSelected(item);
+    switchFragment(item.getItemId());
+    item.setChecked(true);
+    return true;
+  }
+  
+  private void switchFragment(int menuItemId) {
+    Fragment newFragment = fragmentsMap.get(menuItemId);
+    if (newFragment == null) {
+      switch (menuItemId) {
+        case R.id.drawer_item_timetable:
+          newFragment = new TimetableFragment();
+          fragmentsMap.put(menuItemId, newFragment);
+          break;
+        default:
+          Toast.makeText(this, "Not Implemented", Toast.LENGTH_SHORT).show();
+          drawer.closeDrawers();
+          return;
+      }
+    }
+    getSupportFragmentManager()
+      .beginTransaction()
+      .replace(R.id.frame_main, newFragment)
+      .commit();
+    drawer.closeDrawers();
   }
 }
--- app/src/main/java/szebra/senshu_timetable/activities/TimetableFragment.java
+++ app/src/main/java/szebra/senshu_timetable/activities/TimetableFragment.java
@@ -1,9 +1,10 @@
 package szebra.senshu_timetable.activities;
 
-import android.app.Fragment;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
